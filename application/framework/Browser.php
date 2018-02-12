1a482c39287bc037f995399dae650126a9e7dfea
Demyanova, Vasilisa <v.demyanova@a1qa.com>
2018-02-12 18:01:41 +0300

    fix ip adress

--- application/framework/Browser.php
+++ application/framework/Browser.php
@@ -4,6 +4,7 @@ namespace Application\Framework;
 
 use Application\Framework\Firefox;
 use Application\Framework\Chrome;
+use PHPUnit\Framework\TestCase;
 
 /**
  * Class Browser
@@ -16,8 +17,15 @@ class Browser
     public static function getMyDriver()
     {
         $browserType = getenv(self::ENV_NAME_BROWSER_TYPE);
-        self::$host = $GLOBALS["serverHost"];
-
+//        $isRemote = getenv("isRemote"); //$GLOBALS["serverHost"];
+//        TestCase::assertTrue($isRemote, "isRemote=" . $isRemote);
+//        if( $isRemote == True){
+//        self::$host = $GLOBALS["serverHost"];
+//        }
+//        else{
+//            self::$host = "http://localhost:4444/wd/hub";
+//        }
+        self::$host = getenv($GLOBALS["serverHost"]);
         switch ($browserType) {
             case "chrome":
                 {
