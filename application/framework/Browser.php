4d8aae19a57c5951271e4a610d043aeab3e0557c
Demyanova, Vasilisa <v.demyanova@a1qa.com>
2018-02-12 18:34:13 +0300

    fix ip adress

--- application/framework/Browser.php
+++ application/framework/Browser.php
@@ -19,6 +19,7 @@ class Browser
         $browserType = getenv(self::ENV_NAME_BROWSER_TYPE);
         self::$host = $GLOBALS["serverHostLocal"];
         $isRemote = getenv("isRemote");
+//        TestCase::assertTrue($isRemote, "isRemote=" . $isRemote);
         if( $isRemote == True){
         self::$host = $GLOBALS["serverHostVM"];
         }
